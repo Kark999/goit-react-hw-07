9e1c6dfdd89c93c48ec924e06112b398d279cd3d
Andrey Svyrskyi <akkad1v@gmail.com>
2024-04-24 04:37:33 +0300

    'update'

--- src/redux/contactsOps/contactsOps.js
+++ src/redux/contactsOps/contactsOps.js
@@ -1,4 +1,5 @@
-import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
 
 export const INITIAL_STATE = {
   contacts: {
@@ -11,22 +12,40 @@ export const INITIAL_STATE = {
   },
 };
 
-const contactsOpsSlice = createSlice({
-  name: "contactsOpsSlice",
-  initialState: INITIAL_STATE.contacts,
+axios.defaults.baseURL = "https://6627e2e1b625bf088c0a2892.mockapi.io/";
 
-  reducers: {
-    addContact(state, action) {
-      state.items.push(action.payload);
-    },
-    deleteContact(state, action) {
-      state.items = state.items.filter(
-        (contact) => contact.id !== action.payload
-      );
-    },
-  },
-});
+export const fetchContacts = createAsyncThunk(
+  "contacts/fetchAll",
+  async (contacts, thunkAPI) => {
+    try {
+      const { data } = await axios.get("/contacts");
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
 
-export const { addContact, deleteContact } = contactsOpsSlice.actions;
+export const addContact = createAsyncThunk(
+  "contacts/addContact",
+  async (contact, thunkAPI) => {
+    try {
+      const { data } = await axios.post("/contacts", contact);
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
 
-export const contactsReducer = contactsOpsSlice.reducer;
+export const deleteContact = createAsyncThunk(
+  "contacts/deleteContact",
+  async (id, thunkAPI) => {
+    try {
+      const { data } = await axios.delete(`/contacts/${id}`);
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
--- src/redux/contactsSlice/contactsSlice.js
+++ src/redux/contactsSlice/contactsSlice.js
@@ -1,22 +1,59 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { INITIAL_STATE } from "../contactsOps/contactsOps";
+import {
+  INITIAL_STATE,
+  addContact,
+  deleteContact,
+  fetchContacts,
+} from "../contactsOps/contactsOps";
 
 const contactsSlice = createSlice({
   name: "contactsSlice",
   initialState: INITIAL_STATE.contacts,
 
-  reducers: {
-    addContact(state, action) {
-      state.items.push(action.payload);
-    },
-    deleteContact(state, action) {
-      state.items = state.items.filter(
-        (contact) => contact.id !== action.payload
-      );
-    },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchContacts.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchContacts.fulfilled, (state, action) => {
+        state.items = action.payload;
+        state.loading = false;
+        state.error = null;
+      })
+      .addCase(fetchContacts.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      .addCase(addContact.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(addContact.fulfilled, (state, action) => {
+        state.items.push(action.payload);
+        state.loading = false;
+        state.error = null;
+      })
+      .addCase(addContact.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      .addCase(deleteContact.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(deleteContact.fulfilled, (state, action) => {
+        state.items = state.items.filter(
+          (contact) => contact.id !== action.payload
+        );
+        state.loading = false;
+        state.error = null;
+      })
+      .addCase(deleteContact.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
   },
 });
-
-export const { addContact, deleteContact } = contactsSlice.actions;
 
 export const contactsReducer = contactsSlice.reducer;
